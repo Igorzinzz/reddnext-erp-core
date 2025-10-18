e66b63afa0977a0b95b5a7a97736241d40c0840f
Igorzinzz <igorbeltramellooficial@gmail.com>
2025-10-18 22:31:51 +0000

    Adicionado bate-papo de suporte

--- core/footer.php
+++ core/footer.php
@@ -28,3 +28,31 @@
         });
     });
 </script>
+
+<?php
+// ===========================
+// Oculta o Tawk.to apenas no módulo PDV
+// ===========================
+$currentPath = $_SERVER['REQUEST_URI'];
+$isPDV = (strpos($currentPath, '/modulos/pdv') !== false);
+
+// Se NÃO estiver no PDV, carrega o chat
+if (!$isPDV):
+?>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/68f411030524d4194f53317d/1j7sm4tub';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+<?php endif; ?>
+
+</body>
+</html>
